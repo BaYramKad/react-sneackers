11f307bd0690616439df9ec7e6966dcdc64db4bd
Bayram <bayram.kad@gmail.com>
2021-08-09 16:55:46 +0300

    fixed bags

--- src/App.js
+++ src/App.js
@@ -100,11 +100,11 @@ function App() {
           <Header onShowCart={() => setCart(true)}/>
             <Drawer onCloseCart={ () => setCart(false)} isCart={isCart} removeCard={removeCard} cartItems={cartItems} />
             
-            <Route path='/favorites' exact>
+            <Route path='favorites' exact>
               <Favorites addInToFavorites={addInToFavorites} addItemToCart={addItemToCart}  />  
             </Route>
 
-            <Route path=''>
+            <Route path='' exact>
               <Home
                 items={items}
                 cartItems={cartItems}
@@ -117,7 +117,7 @@ function App() {
                 />
             </Route>
 
-            <Route path='/orders' exact> 
+            <Route path='orders' exact> 
              <Orders />
             </Route>
           </div>
