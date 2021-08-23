5bdeb3f233087c4ad7e528889904d9e2d761fc24
Bayram <bayram.kad@gmail.com>
2021-08-24 00:24:40 +0300

    f

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -24,6 +24,8 @@ function Drawer({ onCloseCart, removeCard, cartItems, isCart}) {
   const [isOrder, setOrder] = React.useState(false)
   const [orders, setOrders] = React.useState([])
 
+
+  console.log(123);
   const changeCart = async () => {
     try {
       const {data} = await axios.post('https://60d5a9d3943aa60017768b17.mockapi.io/orders', { items: cartItems })
